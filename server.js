f6a011eb374b1bc18e5a83a2a6b062dc63088d48
Shlomi Goland <golandsh@gmail.com>
2018-07-21 11:14:28 +0300

    Added projects page

--- server.js
+++ server.js
@@ -48,6 +48,13 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects Page',
+    welcomeMessage: 'Welcome to our new projects page'
+  });
+});
+
 app.get('/bad', (req, res) => {
   res.send({
     name: 'Error',
